e2f9a6300f9809079444870c9f5f0eecbf58fefd
kdavis012218 <35358468+kdavis012218@users.noreply.github.com>
2018-02-25 19:48:53 -0500

    exercise 12

---
+++ Exercise 12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Asterisk = "*";
+            const string Space = " ";
+            const int Counter = 10;
+            for (int r = 0; r < Counter; r++) ;
+        }
+        static public void Display()
+        {
+            int Counter = 0;
+            for (int r = 0; r < Counter; r++) ;
+        }
+        
+
+
+
+
+    }
+}
